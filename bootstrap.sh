9759daa94eaf7fbd6cbf9253e51e2bfe0abfb502
Fidel Abella <fabella42@gmail.com>
2016-03-26 21:51:51 -0500

    added bash_profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,8 @@ git submodule init
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	# source ~/.bash_profile; don't have a bash profile yet
+	source ~/.bash_profile;
+	vim +PluginInstall +qall #install all the vim plugins
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
