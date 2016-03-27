18d9e8d679e1c1738226c2febf79404c60a0e9fe
Fidel Abella <fabella42@gmail.com>
2016-03-27 15:03:52 -0500

    added more dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,19 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
 	source ~/.bash_profile;
-	vim +PluginInstall +qall #install all the vim plugins
+	
+	# Install rust programming language
+	curl -sSf https://static.rust-lang.org/rustup.sh | sh
+	# Install Nodejs 5.x version
 	curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 	sudo apt-get install -y nodejs
-	sudo npm install -g typescript bower
-	sudo apt-get install build-essential cmake python-dev python3-dev mono-complete gccgo-go
+	# Install typescript bower gulp
+	sudo npm install -g typescript bower gulp
+	# Install other dependencies needed for YouCompleteMe
+	sudo apt-get install -y build-essential cmake python-dev python3-dev mono-complete gccgo-go
+	# Install all the vim plugins
+	vim +PluginInstall +qall #install all the vim plugins
+	# Install YouCompleteMe for all languagues
 	cd ~/.vim/bundle/YouCompleteMe
 	./install.py --all
 }
